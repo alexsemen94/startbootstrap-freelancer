c60bd82934715d085f6208f2fc4275e65916819c
aleksey.semenov <aleksey.semenov.fin@gmail.com>
2018-12-23 12:11:00 +0300

    add slider and vidion in popup

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
   });
 
   // Scroll to top button appear
-  $(document).scroll(function() {
+  $(document).scroll(function () {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
@@ -26,7 +26,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -37,7 +37,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -56,20 +56,79 @@
     focus: '#username',
     modal: true
   });
-  $(document).on('click', '.portfolio-modal-dismiss', function(e) {
+
+  var items = {
+    _1: {
+      id: '9obr3HZ7108'
+    },
+    _2: {
+      id: 'NG3hp_nFsB8'
+    }
+  }
+
+  $('.portfolio-item').click(function(e) {
+    e.preventDefault();
+    console.log(items[$(this).attr('data-id')]['id']);
+    onYouTubePlayerAPIReady(items[$(this).attr('data-id')]['id']);
+  });
+
+  $(document).on('click', '.portfolio-modal-dismiss', function (e) {
     e.preventDefault();
     $.magnificPopup.close();
   });
 
   // Floating label headings for the contact form
-  $(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+  $(function () {
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
       $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
+    }).on("focus", ".floating-label-form-group", function () {
       $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
+    }).on("blur", ".floating-label-form-group", function () {
       $(this).removeClass("floating-label-form-group-with-focus");
     });
   });
 
+  var arrImages = [
+    {
+      src: 'img/img1.jpg',
+      alt: ''
+    },
+    {
+      src: 'img/img2.jpg',
+      alt: ''
+    },
+    {
+      src: 'img/img3.jpg',
+      alt: ''
+    }
+  ];
+
+  function initCarousel(arrImages, selector) {
+    var carousel_inner = $(selector);
+    var flagFirst = true;
+    arrImages.map(function (slide) {
+      var content = '<div class="carousel-item ';
+      if (flagFirst) {
+        flagFirst = false;
+        content += 'active';
+      }
+      content += ('">'
+        + '<img class="d-block w-100" src="' + slide.src + '" alt="' + slide.alt + '">'
+        + '</div>');
+      carousel_inner.append(content);
+    });
+  }
+
+  initCarousel(arrImages, '#slider .carousel-inner');
+
+  var player;
+  function onYouTubePlayerAPIReady(id) {
+    player = new YT.Player('teather-1', {
+      height: '360',
+      width: '640',
+      videoId: id
+    });
+  }
+
+  onYouTubePlayerAPIReady();
 })(jQuery); // End of use strict
